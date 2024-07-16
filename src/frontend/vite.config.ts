d2e0e4b3df79334588ddb541c96a1d5e1a4488d1
Baffin Lee <baffinlee@gmail.com>
2024-07-16 07:18:22 +0000

    chore: change host and port

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -6,6 +6,8 @@ import svgLoader from 'vite-svg-loader'
 export default defineConfig({
   plugins: [vue(), svgLoader()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8080',
